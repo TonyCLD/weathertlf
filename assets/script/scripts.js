7fd4e9e9cdf2a6ddaff0bd73393ad8fd8a2a983e
Anthony LoneFight <tonylonefight@gmail.com>
2022-07-11 22:47:04 -0500

    moving into five day forecast

--- assets/script/scripts.js
+++ assets/script/scripts.js
@@ -34,7 +34,39 @@ var ourFetch = fetch(url).then(function (resObject) {
     $('#bigweathericon').attr("src", "assets/images/stormy.gif");
     console.log(rainEl); 
     }
-                                             
+// Begins five day forecast 
+$('#tempf_main1').html(Math.floor(data.daily[0].temp.day));
+$('#relh_main1').html(Math.floor(data.daily[0].humidity));
+$('#UV_main1').html(data.daily[0].wind_speed);
+$('wind_main1').html(data.daily[0].uvi);
+
+console.log(Math.round(data.daily[0].weather[0].id));
+
+    if (Math.round(data.daily[0].weather[0].id) > 801) {
+    $('#5day01').attr("src", "assets/images/cloudy.gif"); 
+    }
+    if (Math.round(data.daily[0].weather[0].id) === 801) {
+    $('#5day01').attr("src", "assets/images/partly.gif"); 
+    }
+    if (data.daily[0].weather[0].id === 800) {
+    $('#5day01').attr("src", "assets/images/sunny.gif"); 
+    }
+    var rainEl = (Math.round(data.daily[0].weather[0].id / 100) * 100);
+    if (rainEl > 299 && rainEl < 322) {
+    $('#5day01').attr("src", "assets/images/rain.gif"); 
+    }
+    if (rainEl > 499 && rainEl < 532) {
+    $('#5day01').attr("src", "assets/images/rain.gif"); 
+    }
+    if (rainEl > 199 && rainEl < 233) {
+    $('#5day01').attr("src", "assets/images/stormy.gif");
+    console.log(rainEl); 
+    }
+
+    $('#tempf_main2').html(Math.floor(data.daily[1].temp.day));
+    $('#relh_main2').html(Math.floor(data.daily[1].humidity));
+    $('#UV_main2').html(data.daily[1].wind_speed);
+    $('wind_main2').html(data.daily[1].uvi);
 
 
 })
